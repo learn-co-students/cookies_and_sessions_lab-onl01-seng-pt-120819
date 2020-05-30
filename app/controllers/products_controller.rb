2684e7888352be833534ab0f8cb4c6b4f05a9a4e
lkorchnoy <lkorchnoy@yahoo.com>
2020-05-30 00:49:56 -0400

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+
+    def index
+        @products = Product.all 
+    end
+
+    def add_to_cart
+        @product = Product.find(params[:id])
+        current_cart << @product.id 
+    end
+    
+    
+    
+end
